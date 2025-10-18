d592edfdb61a18f9e703f982bb30ad20560af6ec
artom_artom <97805658+artomartom@users.noreply.github.com>
2025-10-18 19:07:35 +0300

    Remove main execution block from main.py

--- main.py
+++ main.py
@@ -13,6 +13,4 @@ def hello_name(name):
 @app.route('/about')
 def about():
     return '<p>This is a simple Flask web server</p>'
-if __name__ == "__main__":
-    port = 8080
-    app.run(debug=True,host='0.0.0.0',port=port)
+ 
