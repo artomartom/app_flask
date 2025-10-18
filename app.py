155bbfc92bc856f55cc80f8e380ad9843ac3349f
artom_artom <97805658+artomartom@users.noreply.github.com>
2025-10-18 17:48:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+ from flask import Flask
 
 app = Flask(__name__)
 
@@ -13,6 +13,3 @@ def hello_name(name):
 @app.route('/about')
 def about():
     return '<p>This is a simple Flask web server</p>'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
