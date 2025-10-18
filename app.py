f661ca59bc4707b5142456a5aa22d6ee1efebdf6
artom_artom <97805658+artomartom@users.noreply.github.com>
2025-10-18 16:21:59 +0300

    Initialize a simple Flask web server

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f'<h1>Hello, {name}!</h1>'
+
+@app.route('/about')
+def about():
+    return '<p>This is a simple Flask web server</p>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
